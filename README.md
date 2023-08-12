33af5fe3e4c92cbf347d6a58741f20d4bc800fc4
Raumik <ranaraumik@gmail.com>
2023-08-12 16:00:00 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# utils-three-js
+
+A utility library for working with BufferGeometry in Three.js.
+I have created this library to make it easier to work with BufferGeometry in Three.js.
+
+## Installation
+
+You can install `utils-three-js` using npm:
+
+
+```
+    npm install utils-three-js
+```
+
+## Usage
+
+import { BufferGeoUtils } from 'utils-three-js';
+import * as THREE from 'three';
+
+// Create a BufferGeometry
+const geometry = new THREE.BoxGeometry();
+
+// Check if the geometry is a BufferGeometry
+if (BufferGeoUtils.isBufferGeometry(geometry)) {
+    // Perform operations on the geometry
+    const positionArray = BufferGeoUtils.getPositionArray(geometry);
+    const normalArray = BufferGeoUtils.getNormalArray(geometry);
+    const uvArray = BufferGeoUtils.getUVArray(geometry);
+    const facePoints = BufferGeoUtils.getFacePoints(geometry,10)
+    // ...and more
+}
+
+## API Documentation
+`BufferGeoUtils`
+
+| Method                   | Description                                                          | Parameters                                 | Returns                   |
+|--------------------------|----------------------------------------------------------------------|--------------------------------------------|--------------------------|
+| `isBufferGeometry`       | Checks if the given geometry is a BufferGeometry.                   | `geometry` (THREE.BufferGeometry)           | `Boolean`                |
+| `getPositionArray`       | Returns the position array of the geometry.                          | `geometry` (THREE.BufferGeometry)           | `Array`                  |
+| `getNormalArray`         | Returns the normal array of the geometry.                            | `geometry` (THREE.BufferGeometry)           | `Array`                  |
+| `getUVArray`             | Returns the UV array of the geometry.                                | `geometry` (THREE.BufferGeometry)           | `Array`                  |
+| `getIndicesArray`        | Returns the indices array of the geometry.                           | `geometry` (THREE.BufferGeometry)           | `Array`                  |
+| `getNumberOfFaces`       | Returns the number of faces in the geometry.                         | `geometry` (THREE.BufferGeometry)           | `Number`                 |
+| `getNumberOfVertices`    | Returns the number of vertices in the geometry.                      | `geometry` (THREE.BufferGeometry)           | `Number`                 |
+| `getPoint`               | Returns the point at the given index.                                | `geometry` (THREE.BufferGeometry), `index` (Number) | `THREE.Vector3`           |
+| `getFacePointIndices`    | Returns the indices of the points of the face at the given index.    | `geometry` (THREE.BufferGeometry), `faceIndex` (Number) | `Array[Number]`           |
+| `getFacePoints`          | Returns the points of the face at the given index.                   | `geometry` (THREE.BufferGeometry), `faceIndex` (Number) | `Array[THREE.Vector3]`    |
+
+## License
+
+This project is licensed under the MIT License.
+
+## Keywords
+
+three.js, buffergeometry, geometry, utils, utility, lib
---
+++ index.js
@@ -0,0 +1,200 @@
+import * as THREE from 'three';
+
+class BufferGeoUtils {
+    constructor(inGeometry) {
+        /**
+         * @param {THREE.BufferGeometry} inGeometry
+         */
+        this.geometry = inGeometry;
+    }
+    static isBufferGeometry(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Boolean}
+         * @description Checks if the geometry is a buffer geometry
+         */
+        return Boolean(geometry && geometry.isBufferGeometry);
+    }
+    static throwError(inMsg) {
+        /**
+         * @param {String} inMsg
+         * @description Throws an error
+         * @returns {Error}
+         */
+        throw new Error(inMsg);
+    }
+    static getPositionArray(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Array}
+         * @description Returns the position array of the geometry
+         * @throws {Error} if the geometry is not a buffer geometry
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        const positionAttribute = geometry.getAttribute('position');
+        return positionAttribute.array;
+    }
+    static getNormalArray(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Array}
+         * @description Returns the normal array of the geometry
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the geometry does not have normals
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        const normalAttribute = geometry.getAttribute('normal');
+
+        if (normalAttribute === undefined) {
+            this.throwError('No normal attribute found');
+        }
+        return normalAttribute.array;
+    }
+
+    static getUVArray(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Array}
+         * @description Returns the uv array of the geometry
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the geometry does not have uv coordinates
+         * @throws {Error} if the geometry does not have uv coordinates
+        */
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        const uvAttribute = geometry.getAttribute('uv');
+
+        if (uvAttribute === undefined) this.throwError('No uv attribute found');
+        return uvAttribute.array;
+    }
+
+    static getIndicesArray(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Array}
+         * @description Returns the indices array of the geometry
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the geometry does not have indices
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        const indexAttribute = geometry.getIndex();
+
+        if (indexAttribute === null) this.throwError('No index attribute found');
+
+        return indexAttribute.array;
+    }
+
+    static getNumberOfFaces(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Number}
+         * @description Returns the number of faces of the geometry
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the geometry does not have indices
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        const indexAttribute = geometry.getIndex();
+
+        if (indexAttribute === null) {
+            return BufferGeoUtils.getPositionArray(geometry).length / (3 * 3);
+        }
+        return indexAttribute.count / 3;
+    }
+
+    static getNumberOfVertices(geometry) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @returns {Number}
+         * @description Returns the number of vertices of the geometry
+         * @throws {Error} if the geometry is not a buffer geometry
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        return BufferGeoUtils.getPositionArray(geometry).length / 3;
+    }
+
+    static getPoint(geometry, index) {
+        /**
+         * @param {THREE.BufferGeometry} geometry
+         * @param {Number} index
+         * @returns {THREE.Vector3}
+         * @description Returns the point at the given index
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the index is out of range
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+        
+        let attribute = geometry.getAttribute('position');
+
+        // Check if index is in range
+        if (index >= attribute.count || index < 0) {
+            this.throwError('Index out of range');
+        }
+        return new THREE.Vector3(
+            attribute.array[index * 3],
+            attribute.array[index * 3 + 1],
+            attribute.array[index * 3 + 2]
+        );
+    }
+
+    static getFacePointIndices(geometry, faceIndex) {
+        /**
+         * @param {THREE.BufferGeometry}
+         * @param {Number} faceIndex
+         * @returns {Array[Number]}
+         * @description Returns the indices of the points of the face at the given index
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the index is out of range
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        const indexAttribute = geometry.getIndex();
+
+        // Check if index is in range
+        if (faceIndex >= BufferGeoUtils.getNumberOfFaces(geometry) || faceIndex < 0) {
+            this.throwError('Index out of range');
+        }
+
+        if (indexAttribute === null) {
+            return [faceIndex * 3, (faceIndex * 3) + 1, (faceIndex * 3) + 2 ];
+        }
+        let array = indexAttribute.array;
+        return [array[faceIndex * 3], array[(faceIndex * 3) + 1], array[(faceIndex * 3) + 2]];
+    }
+
+    static getFacePoints(geometry, faceIndex) {
+        /**
+         * @param {THREE.BufferGeometry}
+         * @param {Number} faceIndex
+         * @returns {Array[THREE.Vector3]}
+         * @description Returns the points of the face at the given index
+         * @throws {Error} if the geometry is not a buffer geometry
+         * @throws {Error} if the index is out of range
+         * @throws {Error} if the geometry does not have indices
+         */
+
+        if (!BufferGeoUtils.isBufferGeometry(geometry)) this.throwError('Not a buffer geometry')
+
+        let indices = BufferGeoUtils.getFacePointIndices(geometry, faceIndex);
+
+        let points = [];
+        for (let i = 0; i < indices.length; i++) {
+            points.push(BufferGeoUtils.getPoint(geometry, indices[i]));
+        }
+
+        return points;
+    }
+}
+
+export { BufferGeoUtils };
